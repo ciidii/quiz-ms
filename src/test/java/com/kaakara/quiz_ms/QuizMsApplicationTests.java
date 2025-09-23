313060db326c6b25229006be3f54ef7be8fb0890
cidi Diallo <booba167967@gmail.com>
2025-09-23 13:39:43 +0000

    initial commit

---
+++ src/test/java/com/kaakara/quiz_ms/QuizMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaakara.quiz_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
