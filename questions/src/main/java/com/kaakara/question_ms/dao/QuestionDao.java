e1d3f554205221f669fa6264bf6e17c37d172a54
cidi Diallo <booba167967@gmail.com>
2025-09-24 11:25:49 +0000

    feat: question microservices added

---
+++ questions/src/main/java/com/kaakara/question_ms/dao/QuestionDao.java
@@ -0,0 +1,19 @@
+package com.kaakara.question_ms.dao;
+
+
+
+import com.kaakara.question_ms.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategoryIgnoreCase(String category);
+
+    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionsByCategory(String category, int numQ);
+}
