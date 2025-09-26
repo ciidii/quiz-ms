b26f7362580749cd997cb6b2db2eb88bfe28ea8a
cidi Diallo <booba167967@gmail.com>
2025-09-26 11:18:52 +0000

    feat: computer scole done

--- quizservice/src/main/java/com/kakakara/quiz_ms/service/QuizService.java
+++ quizservice/src/main/java/com/kakakara/quiz_ms/service/QuizService.java
@@ -39,26 +39,11 @@ public class QuizService {
         Quiz quiz = quizDao.findById(id).get();
         List<Integer> questionsFromDB = quiz.getQuestions();
         List<QuestionWrapper> questionsForUser = this.questionInterface.fetchQuestions(questionsFromDB).getBody();
-//        for(Question q : questionsFromDB){
-//            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
-//            questionsForUser.add(qw);
-//        }
-//
         return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
 
     }
 
     public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
-        Quiz quiz = quizDao.findById(id).get();
-//        List<Question> questions = quiz.getQuestions();
-        int right = 0;
-//        int i = 0;
-//        for(Response response : responses){
-//            if(response.getResponse().equals(questions.get(i).getRightAnswer()))
-//                right++;
-//
-//            i++;
-//        }
-        return new ResponseEntity<>(right, HttpStatus.OK);
+        return this.questionInterface.computeScore(responses);
     }
 }
